37ccfda36633bd901b9e42d4c4501ed2b3968632
Charleskojomark <markcharleskojo@gmail.com>
2024-12-03 11:30:47 +0100

    update

--- symptosync/settings.py
+++ symptosync/settings.py
@@ -83,12 +83,7 @@ WSGI_APPLICATION = 'symptosync.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # if DEBUG:  # Development: Use SQLite
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# !
 
 DATABASES = {
 'default': {
